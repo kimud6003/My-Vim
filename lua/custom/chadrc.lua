e208d0817de64845841713af462f5326847cf79e
kimud6003 <kimud6003@gmail.com>
2022-05-23 16:13:06 +0900

    First Commit

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,15 @@
+-- Just an example, supposed to be placed in /lua/custom/
+
+local M = {}
+
+-- make sure you maintain the structure of `core/default_config.lua` here,
+-- example of changing theme:
+
+M.ui = {
+  theme = "tokyonight",
+    lspconfig = {
+    setup_lspconf = "custom.plugins.lspconfig",
+  },
+}
+
+return M
---
+++ lua/custom/init.lua
@@ -0,0 +1,4 @@
+-- example file i.e lua/custom/init.lua
+
+-- load your globals, autocmds here or anything .__.
+require'lspconfig'.pyright.setup{}
