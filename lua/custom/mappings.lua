1911a9c0d7533aca99d6ca159862df55537ee0a2
kimud6003 <kimud6003@gmail.com>
2022-07-14 23:16:02 +0900

    window move key Set

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -20,7 +20,11 @@ M.gitsings = {
 M.general = {
    n = {
       [":"] = {"<cmd>FineCmdline<CR>","FineCmdline"},
-      ["/"] = {"<cmd>SearchBoxIncSearch<CR>","SearchBoxIncSearch"}
+      ["/"] = {"<cmd>SearchBoxIncSearch<CR>","SearchBoxIncSearch"},
+      ["<S-h>"] = { "<C-w>h", " window left" },
+      ["<S-l>"] = { "<C-w>l", " window right" },
+      ["<S-j>"] = { "<C-w>j", " window down" },
+      ["<S-k>"] = { "<C-w>k", " window up" },
    }
 }
 
